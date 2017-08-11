683fef8156b68a41f8e5f70bfaf65f745ef66b92
Artur Daukaev <mckorven@gmail.com>
2017-08-11 23:43:44 +0500

    Add legth validation for name

--- app.rb
+++ app.rb
@@ -7,7 +7,10 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base
-	validates :name, :phone, :datestamp, :color, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
+	validates :phone, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
 
 class Barber < ActiveRecord::Base
